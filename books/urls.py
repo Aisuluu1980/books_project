5b17b37a898beb2199b6e8cf978f58277d0b9402
Aisuluu1980 <aisuluueco2009@yandex.ru>
2021-01-19 00:03:21 +0600

    Created books.html and added books list in books.html.HW#32

--- books/urls.py
+++ books/urls.py
@@ -17,11 +17,12 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from main.views import books
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('', homepage, name='home'),
+    path('', books, name='books_index'),
 
 
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) \
--- main/views.py
+++ main/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Books
 
 # Create your views here.
+
+
+def books(request):
+    books_list = Books.objects.all()
+    return render(request, 'books.html', {"books_list": books_list})
