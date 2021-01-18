f8686a132bbccc9156cb3d4871fe27f707988955
Aisuluu1980 <aisuluueco2009@yandex.ru>
2021-01-18 23:39:35 +0600

    Created model Books, migrate and admin.

--- main/models.py
+++ main/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Books(models.Model):
+    title = models.CharField(max_length=80)
+    subtitle = models.CharField(max_length=120)
+    description = models.TextField(max_length=1000)
+    price = models.IntegerField(max_length=6)
+    genre = models.CharField(max_length=50)
+    author = models.CharField(max_length=150)
+    year = models.CharField(max_length=4)
+    date = models.DateField(auto_now=True)
